aa2365b2c247332b20312792adce1fb504a322c6
Vitor Klein <vitorhklein2@gmail.com>
2021-04-21 23:52:02 -0300

    add axios and configure url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,34 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next'
+import { api } from '../services/api'
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  //...
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
    <div>
      <h1>Content</h1>
      <p>{JSON.stringify(props.episodes)}</p>
-   </div>
-   
+   </div> 
  )
 }
 
-export async function getStaticProps() {  
- const response = await fetch('http://localhost:3333/episodes')
- const data = await response.json()
+export  const getStaticProps: GetStaticProps = async () => {  
+ const { data } = await api.get('episodes',{
+   params: {
+     _limit: 12,
+     _sort: 'published_at',
+     _order: 'desc'
+   }
+ })
 
  return {
    props: {
