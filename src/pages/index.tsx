f512f5f157a54a5ee84406c0e42bb21990ba7374
Vitor Klein <vitorhklein2@gmail.com>
2021-04-22 00:17:52 -0300

    format data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { GetStaticProps } from 'next'
+import{ format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { api } from '../services/api'
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
 
 type Episode = {
   id: string;
   title: string;
+  thumbnail: string;
   members: string;
-  //...
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
 }
 
 type HomeProps = {
@@ -30,9 +37,26 @@ export  const getStaticProps: GetStaticProps = async () => {
    }
  })
 
+ const episodes = data.map((episode) => {
+  return {
+    id: episode.id,
+    title: episode.title,
+    thumbnail: episode.thumbnail,
+    members: episode.members,
+    publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+      locale: ptBR,
+    }),
+    duration: Number(episode.file.duration),
+    durationAsString: convertDurationToTimeString(
+      Number(episode.file.duration)
+    ),
+    url: episode.file.url,
+  };
+});
+
  return {
    props: {
-     episodes: data,
+     episodes,
    },
    revalidate: 60 * 60 * 8,
  }
