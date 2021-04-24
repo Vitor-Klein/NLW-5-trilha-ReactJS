90efd4528953e06375d6a98667ddf359015f573c
Vitor Klein <vitorhklein2@gmail.com>
2021-04-24 00:26:54 -0300

    refatore contexts and add typing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,40 +2,21 @@ import '../styles/global.scss'
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
-import { PlayerContext } from '../contexts/PlayerContexts';
 
 import styles from '../styles/app.module.scss';
-import { useState } from 'react';
+import { PlayerContextProvider } from '../contexts/PlayerContexts';
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false)
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying)
-  }
-
-  function setPlayingState(state: boolean){
-    setIsPlaying(state)
-  }
-
-  return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay,setPlayingState }}>
-      <div className={styles.wrapper}>
-        <main>
-        <Header />
-        <Component {...pageProps} />
-        </main>
-        <Player />
-      </div>
-    </PlayerContext.Provider>
+    return(
+      <PlayerContextProvider>
+        <div className={styles.wrapper}>
+          <main>
+          <Header />
+          <Component {...pageProps} />
+          </main>
+          <Player />
+        </div>
+      </PlayerContextProvider>
   )
 }
 
